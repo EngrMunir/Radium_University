cafe53af5319783270b71c637fdfc334fbfbc768
munir <smsirajulmonir@gmail.com>
2024-12-14 21:07:41 +0600

    route created

--- src/app/modules/academicFaculty/academicFaculty.Validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.Validation.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 
-const academicFacultyValidationSchema = z.object({
+const createAcademicFacultyValidationSchema = z.object({
+    name: z.string({
+        invalid_type_error:'Academic faculty must be string',
+    }),
+});
+const updateAcademicFacultyValidationSchema = z.object({
     name: z.string({
         invalid_type_error:'Academic faculty must be string',
     }),
 });
 
-export const AcademicFacultyValidations={
-    academicFacultyValidationSchema
+export const AcademicFacultyValidation={
+    createAcademicFacultyValidationSchema,
+    updateAcademicFacultyValidationSchema
 }
---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.Validation';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+
+const router = express.Router();
+
+router.post(
+    '/create-academic-faculty',
+    validateRequest(
+        AcademicFacultyValidation.createAcademicFacultyValidationSchema
+    ),
+    AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get(
+    '/:facultyId',
+    AcademicFacultyControllers.getSingleAcademicSemester
+);
+
+router.patch(
+    '/:facultyId',
+    validateRequest(
+        AcademicFacultyValidation.updateAcademicFacultyValidationSchema
+    ),
+    AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+router.get('/',AcademicFacultyControllers.getAllAcademicFaculties);
+
+export const AcademicFacultyRoutes = router;
