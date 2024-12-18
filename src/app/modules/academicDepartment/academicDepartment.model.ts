aec1686556303d3321a1d56bc34524b8164c3782
munir <smsirajulmonir@gmail.com>
2024-12-18 08:13:27 +0600

    transaction and rollback explored

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from "mongoose";
 import { TAcademicDepartment } from "./academicDepartment.interface";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     {
@@ -16,22 +18,26 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     {
         timestamps:true,
     }
-);
+) ;
+
+// pre hook middleware
 
 academicDepartmentSchema.pre('save', async function (next) {
     const isDepartmentExist = await AcademicDepartment.findOne({name:this.name})
     if(isDepartmentExist){
-        throw new Error('This department is already exists!');
+        throw new AppError(httpStatus.NOT_FOUND,'This department is already exists!');
     } 
     next();
 })
+
+
 
 academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
     const query = this.getQuery();
 
     const isDepartmentExist = await AcademicDepartment.findOne({query})
     if(!isDepartmentExist){
-        throw new Error('This department does not exist')
+        throw new AppError(httpStatus.NOT_FOUND,'This department does not exist')
     }
     next()
 })
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import config from "../../config";
 import { AcademicSemester } from "../academicSemester/academicSemester.model";
 import { TStudent } from "../student/student.interface";
@@ -5,6 +6,9 @@ import { Student } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
 import { generateStudentId } from "./user.utils";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
+import { error } from "console";
 
 const createStudentIntoDB = async ( password:string, payload: TStudent) =>{
     
@@ -19,22 +23,44 @@ const createStudentIntoDB = async ( password:string, payload: TStudent) =>{
 
   // find academic semester info
   const admissionSemester = await AcademicSemester.findById(payload.admissionSemester)
+  if (!admissionSemester) {
+    throw new Error("Admission semester not found");
+  }
+  
 
-    // set manually id
-    userData.id = await generateStudentId(admissionSemester)
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+     // set manually id
+     userData.id = await generateStudentId(admissionSemester)
 
-    // create a user
-    const newUser = await User.create(userData); //built in static method
+     // create a user (transaction-1)
+     const newUser = await User.create([userData],{session}); //array
+ 
+     // create a student
+     if(!newUser.length){
+        throw new AppError(httpStatus.BAD_REQUEST,'Failed to create user')
 
-    // create a student
-    if( Object.keys(newUser).length){
-      // set id _id as user
-      payload.id = newUser.id;
-      payload.user = newUser._id; //reference _id
-
-      const newStudent = await Student.create(payload)
-      return newStudent;
-    }
+     }
+      
+       // set id _id as user
+       payload.id = newUser[0].id;
+       payload.user = newUser[0]._id; //reference _id
+ 
+      // create a student 2nd transaction
+       const newStudent = await Student.create([payload],{session});
+       if(!newStudent){
+        throw new AppError(httpStatus.BAD_REQUEST,'Failed to create student');
+       }
+       await session.commitTransaction()
+       await session.endSession()
+       return newStudent;
+     }  
+   catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
+  
   }
 
   export const UserServices ={
