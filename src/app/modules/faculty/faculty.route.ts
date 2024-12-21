0cfb05f94803483271f09a9ce85ed8cd4559a73b
munir <smsirajulmonir@gmail.com>
2024-12-21 08:37:35 +0600

    admin added

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,17 +1,19 @@
-import express  from "express";
-import validateRequest from "../../middlewares/validateRequest";
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { FacultyControllers } from './faculty.controller';
+import { updateFacultyValidationSchema } from './faculty.validation';
 
 const router = express.Router();
 
 router.get('/:id', FacultyControllers.getSingleFaculty);
 
 router.patch(
-    '/:id',
-    validateRequest(updateFacultyValidationSchema),
-    FacultyControllers.updateFaculty,
+  '/:id',
+  validateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
 );
 
-router.delete('/:id',FacultyControllers.deleteFaculty);
+router.delete('/:id', FacultyControllers.deleteFaculty);
 
 router.get('/', FacultyControllers.getAllFaculties);
 
